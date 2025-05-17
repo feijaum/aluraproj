d17907782390c1873a72fba4823023a94dfd089b
feijaum <101186354+feijaum@users.noreply.github.com>
2025-05-17 17:59:03 -0300

    Create Bibliotecas.bat
    para a execução fora do colab execute esse arquivo como administrador

---
+++ Bibliotecas.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Instalando as bibliotecas necessárias...
+pip install ipywidgets
+pip install fpdf==1.7.2
+pip install google-generativeai
+pip install google-colab
+echo Instalação concluída! Pode executar o seu script Python agora.
+pause
